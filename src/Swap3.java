9a54b1b375a392709656606796a460d64a9f8c8b
RicoBSJ <aubrun.eric@free.fr>
2019-01-20 00:39:46 +0100

    3 Commit Home 20_01

--- src/Swap3.java
+++ src/Swap3.java
@@ -7,23 +7,19 @@ class Swap3 {
 
 		System.out.print("Entrez la premiere  valeur : ");
 		int a = clavier.nextInt();
+
 		System.out.print("Entrez la deuxieme  valeur : ");
 		int b = clavier.nextInt();
+
 		System.out.print("Entrez la troisieme valeur : ");
 		int c = clavier.nextInt();
-		System.out.println("Les valeurs entrees sont : a = " + a + ", b = " + b + " et c = " + c);
+		
+		System.out.println("Les valeurs entrees sont : a = " + a
+							+ ", b = " + b + " et c = " + c);
+		
 		System.out.println("Permutation: a ==> b, b ==> c, c ==> a");
-		/*******************************************
-		 * Completez le programme a partir d'ici.
-		 *******************************************/
-		int d = a;
-		int e = b;
-		a = c;
-		b = d;
-		c = e;
-		/*******************************************
-		 * Ne rien modifier apres cette ligne.
-		 *******************************************/
-		System.out.println("Les valeurs permutees sont : a = " + a + ", b = " + b + " et c = " + c);
+		int d = a;int e = b;a = c;b = d;c = e;
+		System.out.println("Les valeurs permutees sont : a = " + a
+							+ ", b = " + b + " et c = " + c);
 	}
 }
