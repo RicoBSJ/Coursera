2373a53783296c373adca7020ae6e6552e936e81
AUBRUNE <AUBRUNE@P520.ari.asso.fr>
2019-01-21 13:05:36 +0100

    1 Commit Job 21_01

--- src/Champi.java
+++ src/Champi.java
@@ -1,24 +1,43 @@
+import java.util.Scanner;
 
 public class Champi {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
 		System.out.println("Pensez a un champignon : amanite tue mouches, pied bleu, girolle,");
-		System.out.println("CÃ¨pe de Bordeaux, coprin chevelu ou agaric jaunissant.");
+		System.out.println("Cèpe de Bordeaux, coprin chevelu ou agaric jaunissant.");
 
 		System.out.print("Est-ce que votre champignon a des lamelles (true : oui, false : non) ? ");
+		boolean lamelles = sc.nextBoolean();
+		System.out.print("Est-ce que votre champignon vit en forêt (true : oui, false : non) ? ");
+		boolean foret = sc.nextBoolean();
 		System.out.print("Est-ce que votre champignon a un anneau (true : oui, false : non) ? ");
+		boolean anneau = sc.nextBoolean();
 		System.out.print("Est-ce que votre champignon a un chapeau convexe (true : oui, false : non) ? ");
-		System.out.print("Est-ce que votre champignon vit en forÃªt (true : oui, false : non) ? ");
-
-		System.out.print("==> Le champignon auquel vous pensez est ");
-
-		System.out.print("l'agaric jaunissant");
-		System.out.print("l'amanite tue-mouches");
-		System.out.print("le cÃ¨pe de Bordeaux");
-		System.out.print("le coprin chevelu");
-		System.out.print("la girolle");
-		System.out.print("le pied bleu");
-
+		boolean chapeau = sc.nextBoolean();
+		
+		if (lamelles == false && foret == false) {
+			System.out.print("==> Le champignon auquel vous pensez est ");
+			System.out.print("le cèpe de Bordeaux");
+		}
+			if (chapeau == false && lamelles == true && anneau == true) {
+				System.out.print("==> Le champignon auquel vous pensez est ");
+				System.out.print("le coprin chevelu");
+			}
+				if (chapeau == true && lamelles == true && anneau == true) {
+					System.out.print("==> Le champignon auquel vous pensez est ");
+					System.out.print("l'agaric jaunissant");
+				}
+					if (foret == true && lamelles == true) {
+						System.out.print("==> Le champignon auquel vous pensez est ");
+						System.out.print("la girolle");
+					}
+	    	sc.close();
+		}
+//		System.out.print("le cèpe de Bordeaux");
+//		System.out.print("le coprin chevelu");
+//		System.out.print("l'agaric jaunissant");
+//		System.out.print("la girolle");
+//		System.out.print("l'amanite tue-mouches");
+//		System.out.print("le pied bleu");
 	}
-
-}
--- src/Velo.java
+++ src/Velo.java
@@ -4,7 +4,7 @@ public class Velo {
 
     public static void main(String[] args) {
         Scanner clavier = new Scanner(System.in);
-        System.out.print("Donnez l'heure de dÃ©but de la location (un entier) : ");
+        System.out.print("Donnez l'heure de début de la location (un entier) : ");
         int debut = clavier.nextInt();
         System.out.print("Donnez l'heure de fin de la location (un entier) : ");
         int fin = clavier.nextInt();
@@ -16,13 +16,13 @@ public class Velo {
         int coutLocation = 0;
         
         if (debut<0 || debut>24) {
-        	System.out.println("Les heures doivent Ãªtre comprises entre 0 et 24 !");
+        	System.out.println("Les heures doivent être comprises entre 0 et 24 !");
         }
         	if (dureeLocation<1) {
-        		System.out.println("Bizarre, vous n'avez pas louÃ© votre vÃ©lo bien longtemps !");
+        		System.out.println("Bizarre, vous n'avez pas loué votre vélo bien longtemps !");
         	}
         		if (fin<debut) {
-        			System.out.println("Bizarre, le dÃ©but de la location est aprÃ¨s la fin ...");
+        			System.out.println("Bizarre, le début de la location est après la fin ...");
         		}
         
         if (debut >= 17 || debut < 7) {
@@ -34,8 +34,8 @@ public class Velo {
             	coutLocation = dureeLocation * tarifJour;
             	tarifHoraire = tarifJour;
         	}
-    	System.out.println("Vous avez louÃ© votre vÃ©lo pendant "+dureeLocation+" heures au tarif horaire de "+tarifHoraire+" franc(s)");
-        System.out.print("Le montant total Ã  payer est de ");
+    	System.out.println("Vous avez loué votre vélo pendant "+dureeLocation+" heures au tarif horaire de "+tarifHoraire+" franc(s)");
+        System.out.print("Le montant total à payer est de ");
         System.out.println(coutLocation + " franc(s).");
         clavier.close();
         }
