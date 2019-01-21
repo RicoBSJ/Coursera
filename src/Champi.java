4d1e80031120adb4f6a87519e06bad0331bc9d11
AUBRUNE <AUBRUNE@P520.ari.asso.fr>
2019-01-21 16:08:56 +0100

    2 Commit Job 21_01

--- src/Champi.java
+++ src/Champi.java
@@ -7,37 +7,37 @@ public class Champi {
 		System.out.println("Pensez a un champignon : amanite tue mouches, pied bleu, girolle,");
 		System.out.println("Cèpe de Bordeaux, coprin chevelu ou agaric jaunissant.");
 
-		System.out.print("Est-ce que votre champignon a des lamelles (true : oui, false : non) ? ");
-		boolean lamelles = sc.nextBoolean();
+		System.out.print("Est-ce que votre champignon a un chapeau convexe (true : oui, false : non) ? ");
+		boolean chapeau = sc.nextBoolean();
 		System.out.print("Est-ce que votre champignon vit en forêt (true : oui, false : non) ? ");
 		boolean foret = sc.nextBoolean();
 		System.out.print("Est-ce que votre champignon a un anneau (true : oui, false : non) ? ");
 		boolean anneau = sc.nextBoolean();
-		System.out.print("Est-ce que votre champignon a un chapeau convexe (true : oui, false : non) ? ");
-		boolean chapeau = sc.nextBoolean();
-		
-		if (lamelles == false && foret == false) {
+
+		if (chapeau == false && foret == true && anneau == false) {
 			System.out.print("==> Le champignon auquel vous pensez est ");
 			System.out.print("le cèpe de Bordeaux");
 		}
-			if (chapeau == false && lamelles == true && anneau == true) {
+			if (chapeau == false && foret == false && anneau == true) {
 				System.out.print("==> Le champignon auquel vous pensez est ");
 				System.out.print("le coprin chevelu");
 			}
-				if (chapeau == true && lamelles == true && anneau == true) {
+				if (chapeau == true && foret == false && anneau == true) {
 					System.out.print("==> Le champignon auquel vous pensez est ");
 					System.out.print("l'agaric jaunissant");
 				}
-					if (foret == true && lamelles == true) {
+					if (chapeau == false && foret == true && anneau == false) {
 						System.out.print("==> Le champignon auquel vous pensez est ");
 						System.out.print("la girolle");
 					}
+						if (chapeau == true && foret == true && anneau == true) {
+							System.out.print("==> Le champignon auquel vous pensez est ");
+							System.out.print("l'amanite tue-mouches");
+						}
+							if (chapeau == true && foret == true && anneau == false) {
+								System.out.print("==> Le champignon auquel vous pensez est ");
+								System.out.print("le pied bleu");
+							}
 	    	sc.close();
-		}
-//		System.out.print("le cèpe de Bordeaux");
-//		System.out.print("le coprin chevelu");
-//		System.out.print("l'agaric jaunissant");
-//		System.out.print("la girolle");
-//		System.out.print("l'amanite tue-mouches");
-//		System.out.print("le pied bleu");
 	}
+}
