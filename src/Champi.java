c9e6d5310fbfb78d47a2689e2e2aae3d12141a19
RicoBSJ <aubrun.eric@free.fr>
2019-01-23 22:53:52 +0100

    1 Commit Home 23_01

--- src/Champi.java
+++ src/Champi.java
@@ -5,39 +5,49 @@ public class Champi {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
 		System.out.println("Pensez a un champignon : amanite tue mouches, pied bleu, girolle,");
-		System.out.println("C�pe de Bordeaux, coprin chevelu ou agaric jaunissant.");
+		System.out.println("Cèpe de Bordeaux, coprin chevelu ou agaric jaunissant.");
 
-		System.out.print("Est-ce que votre champignon a un anneau (true : oui, false : non) ? ");
-		boolean anneau = sc.nextBoolean();
+		System.out.print("Est-ce que votre champignon a des lamelles (true : oui, false : non) ? ");
+		boolean lamelles = sc.nextBoolean();
 		System.out.print("Est-ce que votre champignon a un chapeau convexe (true : oui, false : non) ? ");
 		boolean chapeau = sc.nextBoolean();
-		System.out.print("Est-ce que votre champignon vit en for�t (true : oui, false : non) ? ");
-		boolean foret = sc.nextBoolean();
+		System.out.print("Est-ce que votre champignon a un anneau (true : oui, false : non) ? ");
+		boolean anneau = sc.nextBoolean();
 
-		if (anneau == true && chapeau == true || foret == false) {
-			System.out.print("==> Le champignon auquel vous pensez est ");
-			System.out.print("l'agaric jaunissant");
-		}
-			if (anneau == true && chapeau == true && foret == true) {
+		if (lamelles == true || lamelles == false) {
+			if (lamelles == false) {
 				System.out.print("==> Le champignon auquel vous pensez est ");
-				System.out.print("l'amanite tue-mouches");
+				System.out.print("le cèpe de Bordeaux");
 			}
-		if (anneau == false && chapeau == false && foret == true) {
-			System.out.print("==> Le champignon auquel vous pensez est ");
-			System.out.print("le c�pe de Bordeaux");
+				if (lamelles == true && chapeau == true) {
+					if (anneau == true) {
+						System.out.print("==> Le champignon auquel vous pensez est ");
+						System.out.print("l'agaric jaunissant");
+					}
+						if (anneau == false) {
+							System.out.print("==> Le champignon auquel vous pensez est ");
+							System.out.print("le pied bleu");
+						}
+				}
 		}
-		if (anneau == true && chapeau == false && foret == false) {
-			System.out.print("==> Le champignon auquel vous pensez est ");
-			System.out.print("le coprin chevelu");	
-		}
-		if (anneau == false && chapeau == false && foret == true) {
-			System.out.print("==> Le champignon auquel vous pensez est ");
-			System.out.print("la girolle");
-		}
-		if (anneau == false && chapeau == true && foret == true) {
-			System.out.print("==> Le champignon auquel vous pensez est ");
-			System.out.print("le pied bleu");
-		}
+		
+
+//		if (anneau == true && chapeau == true && lamelles == true) {
+//			System.out.print("==> Le champignon auquel vous pensez est ");
+//			System.out.print("l'amanite tue-mouches");
+//		}
+//		if (anneau == false && chapeau == false && lamelles == false) {
+//
+//		}
+//		if (anneau == true && chapeau != true && lamelles == true) {
+//			System.out.print("==> Le champignon auquel vous pensez est ");
+//			System.out.print("le coprin chevelu");	
+//		}
+//		if (anneau != true && chapeau != true && lamelles == true) {
+//			System.out.print("==> Le champignon auquel vous pensez est ");
+//			System.out.print("la girolle");
+//		}
+
 	    	sc.close();
 	}
 }
